9edaf08f489d6519a552f470d4f0f336f149afe2
Boris Sclauzero <boris.sclauzero@zupit.it>
2020-02-14 19:52:04 +0100

    initial setup

---
+++ StringCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
